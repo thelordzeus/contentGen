9d3bab4fcd8386c6cc7c1a112751a5bc60587266
thelordzeus <arnav55125@gmail.com>
2023-01-16 22:24:22 +0530

    fixes

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -21,7 +21,7 @@ export default function BottomBar() {
     const data = await response.json();
     const { output } = data;
     console.log("OpenAi replied...", output.text);
-    setApiOutput(`${output.text}`); 
+    setApiOutput(`${output.text}`);
     setIsGenerating(false);
   };
 
@@ -33,26 +33,23 @@ export default function BottomBar() {
     <section className="block fixed  bottom-0 inset-x-0 z-50 shadow-lg text-white bg-black lg:w-2/5  w-11/12 md:w-3/4 m-auto pb-8">
       <div className=" flow-root">
         <div>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center float-left">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center float-left">
             Clear
           </button>
         </div>
         <div>
           <button
             onClick={() => setOpen(true)}
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center  float-right"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full text-center  float-right"
           >
             Add Post
           </button>
           <Modal show={isOpen} onClose={() => setOpen(false)}>
             <Modal.Header>Generate new post</Modal.Header>
             <Modal.Body>
-              <form class="space-y-6 " action="#">
+              <form className="space-y-6 " action="#">
                 <div>
-                  <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Post Title
                   </label>
                   <textarea
@@ -63,20 +60,22 @@ export default function BottomBar() {
                   />
                 </div>
                 <div>
-                    <a
-                      className={
-                        isGenerating ? "generate-button loading" : "generate-button"
-                      }
-                      onClick={callGenerateEndPoint}
-                    >
-                      <div className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 pt-5">
-                        {isGenerating ? (
-                          <span className="loader"></span>
-                        ) : (
-                          <p>Generate</p>
-                        )}
-                      </div>
-                    </a>
+                  <a
+                    className={
+                      isGenerating
+                        ? "generate-button loading"
+                        : "generate-button"
+                    }
+                    onClick={callGenerateEndPoint}
+                  >
+                    <div className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 pt-5">
+                      {isGenerating ? (
+                        <span className="loader"></span>
+                      ) : (
+                        <p>Generate</p>
+                      )}
+                    </div>
+                  </a>
                 </div>
               </form>
             </Modal.Body>
