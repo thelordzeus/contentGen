6af0d7ef353cf447c0689d88c29a4f033bdc01b2
thelordzeus <arnav55125@gmail.com>
2023-01-16 22:18:12 +0530

    fixes: write.js

--- components/GptResponse.jsx
+++ components/GptResponse.jsx
@@ -1,9 +1,9 @@
 export default function GptResponse() {
   return (
     <div className="space-y-4 mt-4 pb-14">
-      <div class="lg:w-2/5  w-11/12 md:w-3/4 m-auto  p-6 rounded-lg shadow-md bg-blue-500">
+      <div className="lg:w-2/5  w-11/12 md:w-3/4 m-auto  p-6 rounded-lg shadow-md bg-blue-500">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
             test test
           </h5>
         </a>
