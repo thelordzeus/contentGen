2208ecb6edecca8993bd386b9dec72e13c009054
thelordzeus <arnav55125@gmail.com>
2023-01-16 17:54:13 +0530

    feat: added Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="p-[20px] flex  items-center max-w-[600px] m-auto border-b-2 border-white o ">
+      <h1 className="text-red-600 font-bold text-2xl">ContentGen</h1>
+      <div className="  ml-auto z-10">
+        <div className="ml-16 no-underline space-x-3 text-blue-600 font-bold ">
+          <a href="https://thelordzeus.vercel.app/" target="_blank">
+            Developer
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-red-500'>Hello world</h1>
+      <div className="bg-black h-screen">
+        <Head>
+          <title>ContentGen</title>
+          <meta
+            name="description"
+            content="A ChatGPT powered writer which writes your linkidn posts for you."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar />
+      </div>
     </>
-  )
+  );
 }
